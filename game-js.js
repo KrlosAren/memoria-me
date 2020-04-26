7552ccb931e432578dd535aa98218e362d09ac4c
Carlos Lopez <krlosaren@gmail.com>
2020-04-26 12:57:07 -0400

    arreglos del domingo

--- game-js.js
+++ game-js.js
@@ -3,10 +3,6 @@ const colorB = document.getElementById('colorB')
 const colorC = document.getElementById('colorC')
 const colorD = document.getElementById('colorD')
 
-colorD.addEventListener('click', function(){
-    this.classList.add('opacity')
-})
-
 
 const containerGame = document.getElementById('contenedorGame')
 const containerInicio = document.getElementById('contenedorInicio')
@@ -29,12 +25,15 @@ class Juego{
         this.iniciar = this.iniciar.bind(this)
         this.iniciar()
         this.generarSecuencia()
-
+        setTimeout(this.subirNivel()
+        ,500)
     }
 
     iniciar(){
-        this.toggleBtnStart()
+        this.subirNivel = this.subirNivel.bind(this)
         this.elegirColor = this.elegirColor.bind(this)
+        this.toggleBtnStart()
+
         this.nivel = 1
         this.colores = {
             colorA,
@@ -45,9 +44,8 @@ class Juego{
     }   
 
     toggleBtnStart(){
-
         if (btnStart.classList.contains('hide')) {
-            btnStart.classList.remove('hide') 
+            btnStart.classList.remove('hide')
         }else{
             btnStart.classList.add('hide')
             containerGame.classList.remove('hide')
@@ -58,51 +56,97 @@ class Juego{
     generarSecuencia(){
         this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map( n => Math.round(Math.random() * 3) + 1 )  
     }
+    
+    subirNivel(){
+        this.subnivel = 0
+        this.iluminarCuadrados()
+        this.eventoBotones()
+    }
 
     cambiarNumeroAColores(numero){
         switch (numero) {
             case 1:
-                return 'ColorA'
+                return 'colorA'
             case 2:
-                return 'ColorB'    
+                return 'colorB'    
             case 3:
-                return 'ColorC' 
+                return 'colorC' 
             case 4:
-                return 'ColorD' 
+                return 'colorD' 
         }
     }   
 
-    encenderCuadros(){
-        console.log('hola')
-    }
-
-    /*
- 
-   iluminarCuadrados(){
-        for (let i = 0; i < this.nivel; i++) {         
-            console.log(this.cambiarNumeroAColores(this.secuencia[i]))
+    cambiarColoresANumero(color){
+        switch (color) {
+            case 'colorA':
+                    return 1
+            case 'colorB':
+                    return 2
+            case 'colorC':
+                    return 3
+            case 'colorD':
+                    return 4
         }
-       
-    }*/
-
-    iluminarColor(){
-        this.colores[color].classList.add('opacity')
     }
 
-    apagarColor(){
-        this.colores[color].classList.remove('light')
+    encenderSecuencia(){
+        for(let i = 0; i < this.nivel; i++){
+            const color = this.cambiarNumeroAColores(this.secuencia[i])
+            setTimeout(() =>  this.iluminarCuadrados(color), 1000 * i)
+        }
+    }
+
+   iluminarCuadrados(color){
+        this.colores[color].classList.add('opacity')   
+        setTimeout(() => this.apagarColor(color), 350)  
+    }
+
+    apagarColor(color){
+        this.colores[color].classList.remove('opacity')
     }
 
     eventoBotones(){
-        this.colores.colorA.addEventListener('click',this.iluminarColor)
-        this.colores.colorB.addEventListener('click',this.iluminarColor)
-        this.colores.colorC.addEventListener('click',this.iluminarColor)
-        this.colores.colorD.addEventListener('click',this.iluminarColor)
+        this.colores.colorA.addEventListener('click',this.elegirColor)
+        this.colores.colorB.addEventListener('click',this.elegirColor)
+        this.colores.colorC.addEventListener('click',this.elegirColor)
+        this.colores.colorD.addEventListener('click',this.elegirColor)
     }
 
-    elegirColor(){
-        alert('hola')
+    removerEventosBotones(){
+        this.colores.colorA.removeEventListener('click',this.elegirColor)
+        this.colores.colorB.removeEventListener('click',this.elegirColor)
+        this.colores.colorC.removeEventListener('click',this.elegirColor)
+        this.colores.colorD.removeEventListener('click',this.elegirColor)
     }
+
+    elegirColor(ev){
+       const nombreColor = ev.target.dataset.color
+       const numeroColor = this.cambiarColoresANumero(nombreColor)
+       this.iluminarCuadrados(nombreColor)
+       if(numeroColor === this.secuencia[this.subnivel]){
+            this.subirNivel++
+            if(this.subirNivel == this.nivel){
+                this.nivel++
+                this.removerEventosBotones()
+                if(this.nivel == (ULTIMO_NIVEL + 1)){
+                    this.ganaste()
+                }
+            }else{
+                setTimeout(this.subirNivel(), 1000);
+            }
+       }else{
+           this.perdiste()
+       }
+    }
+
+    ganaste(){
+        alert('ganaste')
+    }
+
+    perdiste(){
+        alert('perdiste')
+    }
+
 }
 
 function start(){
@@ -110,7 +154,10 @@ function start(){
 }
 
 
-window.addEventListener('click',function (ev) {
-    console.log(this.ev)
-})
 
+
+
+/*
+window.addEventListener('click',function(ev){
+    this.console.log(ev.target)
+})*/
